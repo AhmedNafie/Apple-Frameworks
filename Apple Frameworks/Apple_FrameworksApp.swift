a0af5b3a2731651e78e4edd7da587f0e60162d99
Ahmed Nafie <101005449+AhmedNafie@users.noreply.github.com>
2024-11-18 15:05:30 +0200

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Didia on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
